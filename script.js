4c9fbb97ecba53f4e039ada0cc1f04230482de9a
Kirill <i63247012@gmail.com>
2024-12-19 14:25:04 +0300

    add: hide preloader and content

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ preloader.src = 'img/hp-blog-spinner-example.png'
 
 const preloaderWrapper = document.createElement('div')
 preloaderWrapper.classList.add('preloader-wrapper')
+
+
 preloaderWrapper.appendChild(preloader)
-
-
 document.body.appendChild(preloaderWrapper)
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    setTimeout(() => {
+        preloaderWrapper.classList.add('hide')
+
+        this.setTimeout(() => {
+            preloaderWrapper.classList.add('none')
+        }, 200)
+    }, 500)
+})
